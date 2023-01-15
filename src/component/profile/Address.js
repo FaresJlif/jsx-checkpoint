8b9a223690aea141e104d83ba71087b4e642f2e0
FaresJlif <fares.jlif@gmail.com>
2023-01-15 15:35:16 +0100

    jsx project

---
+++ src/component/profile/Address.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Address = () => {
+    return <h2>Cite el wafa </h2>;
+};
+export default Address;
---
+++ src/component/profile/FullName.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FullName = () => {
+    return <h1>Fares Jlif
+    </h1>;
+};
+export default FullName;
